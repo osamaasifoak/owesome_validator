6d83c00937c17a758d5b1866bd1b9163750f0333
Osama Asif <osama.asif20@gmail.com>
2020-11-20 11:07:52 +0500

    Documentation and Example improved

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
-# owesome_validator
+# Owesome_validator 🛒
 
-A package for flutter for validation based on pure dart code, it will provide most of common validation like name, email, password, contact etc.
+![pub package](https://img.shields.io/pub/v/owesome_validator?label=owesome_validator&style=plastic)
+
+A flutter package for make your life easy. This package is used for validation.
+
+**Show some ❤️ and star the repo to support the project**
+
+This Package is open for contributions.
+
+## Usage
+
+To use this plugin, add `owesome_validator` as a [dependency in your pubspec.yaml file](https://flutter.io/platform-plugins/).
+
+## Getting Started
+
+- first import a package into your file.
+
+- After importing, we are able to get the built-in methods
+  - Name Validation
+    ```
+         OwesomeValidator.name(name, OwesomeValidator.patternNameOnlyChar);
+    ```
+  - Email Validation ➖
+    ```
+         OwesomeValidator.email(email, OwesomeValidator.patternEmail);
+    ```
+  - Phone Validation ➕
+    ```
+         OwesomeValidator.phone(phone, OwesomeValidator.patternPhone);
+    ```
+  - password
+    ```
+        OwesomeValidator.password(password, OwesomeValidator.passwordMinLen8withCamelAndSpecialChar)
+    ```
+
+ ### Note 
+    You can pass your custom regex for validation. This package will be improved more by time to time  
--- example/main.dart
+++ example/main.dart
@@ -1,36 +1,36 @@
 // How to use this package?
 // Don't worry it's very simple..
 
-// This is the base class for your understanding that what is going on inside this package.
+// This is the base class for your understanding
+// that what is going on inside this package.
 
-class OwesomeValidator {
-  /**
-   * [pattern for validationm, These are the basic validation regex patterns you can add your custom regex by passing it to function]
+import 'package:owesome_validator/owesome_validator.dart';
+
+class Example {
+  /*
+    pattern for validationm, These are the basic validation
+     regex patterns you can add your custom regex by passing it to function
    */
-  static const Pattern patternNameOnlyChar = r'^[A-Za-z ]+(?:[ -][A-Za-z ]+)$';
-  static const Pattern passwordMinLen8withCamelAndSpecialChar =
-      r'^((?=.\d)(?=.[a-z])(?=.[A-Z])(?=.*[\W]).{8,20})';
-  static const Pattern patternEmail =
-      r'^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$';
-  static const Pattern patternPhone = r'^(?:[+0]9)?[0-9]{11}$';
 
-  /// [common functions]
-
+  /// [PROVIDE YOUR VALUE AND PASS THE NAME PATTERN HERE]
   static bool name(String name, String pattern) {
     RegExp regexName = new RegExp(pattern);
     return !regexName.hasMatch(name) ? false : true;
   }
 
+  /// [PROVIDE YOUR VALUE AND PASS THE PASSWORD PATTERN HERE]
   static bool password(String password, String pattern) {
     RegExp regexPassword = new RegExp(pattern);
     return !regexPassword.hasMatch(password) ? false : true;
   }
 
+  /// [PROVIDE YOUR VALUE AND PASS THE EMAIL PATTERN HERE]
   static bool email(String email, String pattern) {
     RegExp regexEmail = new RegExp(pattern);
     return !regexEmail.hasMatch(email) ? false : true;
   }
 
+  /// [PROVIDE YOUR VALUE AND PASS THE PHONE PATTERN HERE]
   static bool phone(String phone, String pattern) {
     RegExp regexPhone = new RegExp(pattern);
     return !regexPhone.hasMatch(phone) ? false : true;
@@ -38,17 +38,39 @@ class OwesomeValidator {
 }
 
 // TEST CASE
-void main() {
-  OwesomeValidator.name("Osama Asif", OwesomeValidator.patternNameOnlyChar);
-  OwesomeValidator.email(
-      "osama.asif20@gmail.com", OwesomeValidator.patternEmail);
-  OwesomeValidator.password(
-      "Osama12@1", OwesomeValidator.passwordMinLen8withCamelAndSpecialChar);
-  OwesomeValidator.phone("+923093234697", OwesomeValidator.patternPhone);
-}
-
-// Yeah the usuage is mentioned above just call the class function and utilize it, here's the one more if you want to add your custom regex so just pass your regex string to the function and rest leave on it.
-
+// Yeah the usuage is mentioned above just call the class
+// function and utilize it, here's the one more if you want to add
+// your custom regex so just pass your regex string to the function
+// and rest leave on it.
 // String myCustomRegexString = r'^[A-Za-z ]+(?:[ _-][A-Za-z ]+)*$';
-
 // OwesomeValidator.name("Osama12@1",myCustomRegexString);
+
+void main() {
+  String name = "Osama Asif";
+  String email = "osama.asif20@gmail.com";
+  String password = "Osama@12345";
+  String phone = "+923092783699";
+  String invalid = "Invalid";
+  String valid = "Valid";
+  if (OwesomeValidator.name(name, OwesomeValidator.patternNameOnlyChar)) {
+    print(valid);
+  } else {
+    print(invalid);
+  }
+  if (OwesomeValidator.email(email, OwesomeValidator.patternEmail)) {
+    print(valid);
+  } else {
+    print(invalid);
+  }
+  if (OwesomeValidator.password(
+      password, OwesomeValidator.passwordMinLen8withCamelAndSpecialChar)) {
+    print(valid);
+  } else {
+    print(invalid);
+  }
+  if (OwesomeValidator.phone(phone, OwesomeValidator.patternPhone)) {
+    print(valid);
+  } else {
+    print(invalid);
+  }
+}
